1a47b3fd71b706b0c4906248591812ff3cacb53c
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-10-07 06:31:45 +0200

    chore: remove unneeded projects

--- apps/rxjs-dos-e2e/src/integration/app.spec.ts
+++
@@ -1,9 +0,0 @@
-import { getGreeting } from '../support/app.po';
-
-describe('rxjs-dos', () => {
-  beforeEach(() => cy.visit('/'));
-
-  it('should display welcome message', () => {
-    getGreeting().contains('Welcome to rxjs-dos!');
-  });
-});
