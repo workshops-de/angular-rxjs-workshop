a0e8905a5fd6ba8385aac4b4928caf3f5e9a8538
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-10-10 22:26:33 +0200

    ref: simplify interval update

--- apps/client/src/app/todos/internals/components/todo-navigation/todo-settings.component.ts
+++ apps/client/src/app/todos/internals/components/todo-navigation/todo-settings.component.ts
@@ -18,7 +18,7 @@ import { Observable } from 'rxjs';
         placeholder="What needs to be done?"
         [value]="settings.pollingInterval"
         (change)="updateInterval($event)"
-        (keyup)="updateIntervalOnEnter($event)"
+        (keyup.enter)="updateInterval($event)"
       />
 
       <div class="todo">
@@ -54,10 +54,5 @@ export class TodoSettingsComponent {
 
   updateInterval(event: Event & { target: { value: string } }) {
     this.todoSettings.update({ pollingInterval: +event.target.value });
-  }
-  updateIntervalOnEnter(event: KeyboardEvent & { target: { value: string } }) {
-    if (event.keyCode === 13) {
-      this.todoSettings.update({ pollingInterval: +event.target.value });
-    }
   }
 }
