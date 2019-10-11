3b06da91ea46929bbd4f1d78787f39d3a8d0a8ca
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-10-11 14:38:00 +0200

    solve--retry-fetching-todos

--- apps/client/src/app/todos/todo.service.ts
+++ apps/client/src/app/todos/todo.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, timer } from 'rxjs';
-import { exhaustMap, map, share, tap } from 'rxjs/operators';
+import { EMPTY, Observable, timer } from 'rxjs';
+import { catchError, exhaustMap, map, retry, share } from 'rxjs/operators';
 import { Toolbelt } from './internals';
 import { Todo, TodoApi } from './models';
 import { TodoSettings } from './todo-settings.service';
@@ -19,8 +19,8 @@ export class TodoService {
   loadFrequently(): Observable<Todo[]> {
     // TODO: Introduce error handled, configured, recurring, all-mighty stream
     return timer(0, 5000).pipe(
-      exhaustMap(() => this.query()),
-      tap({ error: () => this.toolbelt.offerHardReload() }),
+      exhaustMap(() => this.query().pipe(catchError(() => EMPTY))),
+      retry(),
       share()
     );
   }
