1ae491f3b3526b7945662d333f603e0d25022dd6
Gregor Woiwode <gregor.woiwode@gmail.com>
2021-05-06 20:38:51 +0200

    solve--fetch-todos-frequently

--- apps/client/src/app/todos/todo.service.ts
+++ apps/client/src/app/todos/todo.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map, share, tap } from 'rxjs/operators';
+import { Observable, timer } from 'rxjs';
+import { exhaustMap, map, share, tap } from 'rxjs/operators';
 import { Toolbelt } from './internals';
 import { Todo, TodoApi } from './models';
 import { TodoSettings } from './todo-settings.service';
@@ -18,7 +18,8 @@ export class TodoService {
 
   loadFrequently(): Observable<Todo[]> {
     // TODO: Introduce error handled, configured, recurring, all-mighty stream
-    return this.query().pipe(
+    return timer(0, 5000).pipe(
+      exhaustMap(() => this.query()),
       tap({ error: () => this.toolbelt.offerHardReload() }),
       share()
     );
