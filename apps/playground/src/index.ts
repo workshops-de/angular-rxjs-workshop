a0eb86dff46ee8fafc0e2a3ec3f774cfa3166fca
Gregor Woiwode <gregor.woiwode@gmail.com>
2021-05-09 16:48:01 +0200

    solve--push-numbers

--- apps/playground/src/index.ts
+++ apps/playground/src/index.ts
@@ -1,1 +1,20 @@
 import { Observable } from 'rxjs';
+
+playground1();
+
+function playground1() {
+  const stream$ = new Observable((observer) => {
+    observer.next(1);
+    observer.next(2);
+    observer.next(3);
+    observer.next(4);
+    observer.next(5);
+    observer.error(new Error('Something went wrong'));
+    observer.complete();
+  });
+
+  stream$.subscribe({
+    next: (value) => console.log(value),
+    complete: () => console.log('Stream is complete! 😎')
+  });
+}
