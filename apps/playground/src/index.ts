5d6ac9419d8e86cd3cd0bb82fb601a4f580251cc
web-dave <dave@webdave.de>
2021-11-17 10:16:34 +0100

    my observable

--- apps/playground/src/index.ts
+++ apps/playground/src/index.ts
@@ -1,1 +1,40 @@
 import { Observable } from 'rxjs';
+
+const myObs = {
+  subscriptions: [],
+  value: -1,
+  subscribe: function (sub: any) {
+    this.subscriptions.push(sub);
+  },
+  next: function (v: number) {
+    this.value = v;
+    this.subscriptions.forEach((sub) => sub(v));
+  }
+};
+
+myObs.subscribe((data) => console.error(data));
+myObs.next(13);
+
+// Create observable
+const helloWorld$ = new Observable(function subscribe(observer) {
+  observer.next(1);
+  observer.next(2);
+  observer.next(3);
+  //   observer.error(new Error('Help. is doof alles hier'));
+  observer.next(4);
+  observer.complete();
+  observer.next(5);
+});
+
+// Subscribe to an observable
+helloWorld$.subscribe({
+  next(x) {
+    console.log(x);
+  },
+  error(err) {
+    console.error(err);
+  },
+  complete() {
+    console.log('done');
+  }
+});
