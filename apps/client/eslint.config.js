416446acd830bf7b2728dae1a69420444b151cf7
Gregor Woiwode <gregor.woiwode@schleupen.de>
2024-12-12 20:28:56 +0100

    chore: install eslint-plugin

--- apps/client/eslint.config.js
+++ apps/client/eslint.config.js
@@ -12,7 +12,7 @@ module.exports = [
         'error',
         {
           type: 'attribute',
-          prefix: 'app',
+          prefix: 'client',
           style: 'camelCase',
         },
       ],
@@ -20,7 +20,7 @@ module.exports = [
         'error',
         {
           type: 'element',
-          prefix: 'app',
+          prefix: 'client',
           style: 'kebab-case',
         },
       ],
